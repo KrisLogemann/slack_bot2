7a843b5e57549a3681d12f20d934578998ad696a
Kris Logemann <krisrlogemann@gmail.com>
2019-08-23 15:33:43 -0700

    adds env shit

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -3,6 +3,6 @@
 module.exports = {
   'mongod': {
     'connector': 'mongodb',
-    'url': 'mongodb://heroku_hrfg20r0:uoiveldi2otfpt3rmscvkugqml@ds211718.mlab.com:11718/heroku_hrfg20r0',
+    'url': process.env.MONGODB_URI,
   },
 };
